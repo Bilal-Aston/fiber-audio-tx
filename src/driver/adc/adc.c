41d62e41993681ac8d98b6800846bc29b2cdea7a
Bilal-Aston <230413564@aston.ac.uk>
2026-01-22 15:19:28 +0000

    Update adc.c
    Great work - no mistakes.
    
    I have added comments.

--- src/driver/adc/adc.c
+++ src/driver/adc/adc.c
@@ -3,20 +3,18 @@
 int adc_init(void)
 {
 
-ADMUX = 1<<REFS0 | 1<<MUX2 | 1<<MUX1 | 1<<MUX0;
+//select ADC0 and Set voltage reference to AVCC
+ADMUX = 1<<REFS0;
 
+ //Enable interrupts and set Prescale 32
 ADCSRA = 1<<ADEN | 1<<ADIE | 1<<ADPS2 | 1<<ADPS0;
 
-DIDR0 = 0xFF;
-
-
-
-
-
+return 0;
 }
 
 void adc_start(void)
 {
+ //start an adc conversion
  ADCSRA |= 1<<ADSC;
 }
 
